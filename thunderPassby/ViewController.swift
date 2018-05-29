2f8413f09d40358a549b34e1a372851d083eff77
zhihao zhang <zhihaozhang@me.com>
2018-05-29 19:11:12 +0800

    Initial Commit

---
+++ thunderPassby/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  thunderPassby
+//
+//  Created by Chih-Hao on 2018/5/29.
+//  Copyright © 2018年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
